ec8db955c03490b77368a11632a8d3e64decb1df
IndoorCorgi <indoorcorgi@gmail.com>
2022-10-08 11:18:19 +0900

    バージョン表記v1.1に更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cgir',
-    version='1.0',
+    version='1.1',
     description='Libraries and command line tool to control infrared LEDs and receivers on Raspberry Pi',
     author='Indoor Corgi',
     author_email='indoorcorgi@gmail.com',
